93af82c490ae5904379581b42d9836eaa12ff5e7
Muhammad1231 <96456436+Muhammad1231@users.noreply.github.com>
2022-09-08 20:39:12 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+ song1="music.mp3";
+
+
+ function preload()
+ {
+    song1=loadSound("music.mp3");
+    
+ }
+
+function setup(){
+    canvas = createCanvas(600,530);
+    canvas.center();
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,600,500);
+}
+function play()
+ {
+    song1.play();
+ }
